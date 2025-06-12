bc331323fa08618833680225d2dc6457930d541b
rubenruizc <ruben.ruiz@iesnervion.es>
2025-06-12 09:38:10 +0200

    Ejercicio Terminado Completo

--- DAL/clsManejadoraJugadoresDAL.cs
+++ DAL/clsManejadoraJugadoresDAL.cs
@@ -16,7 +16,7 @@ namespace DAL
         /// <br></br>
         /// Precondición: El objeto clsJugador debe contener datos válidos para todas las propiedades requeridas.
         /// <br></br>
-        /// Postcondición: Devuelve el número de filas afectadas (1 si la inserción es exitosa, 0 si falla).
+        /// Postcondición: Devuelve el número de filas afectadas (1 si se ha introducido, 0 si falla, 0 si falla).
         /// </summary>
         /// <param name="jugador">Objeto clsJugador con los detalles del jugador a insertar</param>
         /// <returns>Número de filas afectadas en la base de datos</returns>
